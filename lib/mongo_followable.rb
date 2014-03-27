750b5e70bb908ae4986a621a78586efa0b4ceebd
Joseph Chen <joe1chen@hotmail.com>
2014-03-27 08:14:29 -0700

    Add utility methods to check if mongoid2 or mongoid3.

--- lib/mongo_followable.rb
+++ lib/mongo_followable.rb
@@ -1,5 +1,6 @@
 if defined?(Mongoid) or defined?(MongoMapper)
   require File.join(File.dirname(__FILE__), "mongo_followable/core_ext/string")
+  require File.join(File.dirname(__FILE__), "mongo_followable/core_ext/util")
   require File.join(File.dirname(__FILE__), "mongo_followable/followed")
   require File.join(File.dirname(__FILE__), "mongo_followable/follower")
   require File.join(File.dirname(__FILE__), "../app/models/follow")
