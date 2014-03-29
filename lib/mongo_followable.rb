dc6bda883d70ca4c3d935d3d5e9122fdabdcecee
Joseph Chen <joe1chen@hotmail.com>
2014-03-28 21:31:48 -0700

    Move follow to lib dir

--- lib/mongo_followable.rb
+++ lib/mongo_followable.rb
@@ -2,5 +2,5 @@ require "mongo_followable/core_ext/string"
 require "mongo_followable/util"
 require "mongo_followable/followed"
 require "mongo_followable/follower"
-require File.join(File.dirname(__FILE__), "../app/models/mongo_followable/follow")
+require "mongo_followable/follow"
 require "mongo_followable/features/history"
